a93c4c48f6ebf03288495cbec1aab0aa8555c304
guiwitz <guiwitz@users.noreply.github.com>
2023-03-16 17:55:37 +0100

    add first tests

--- tests/test_pymicrofluidics.py
+++ tests/test_pymicrofluidics.py
@@ -1,2 +1,12 @@
+from pymicrofluidics.mfdesign import Design
+
 def test_something():
     pass
+
+def test_design_add_layer():
+    design = Design()
+
+    design.add_layer('Layer1', {'name':'first_layer','color':1, 'inversion':0})
+    design.add_layer('Layer2', {'name':'sec_layer','color':2, 'inversion':0})
+
+    assert len(design.layers) == 2
